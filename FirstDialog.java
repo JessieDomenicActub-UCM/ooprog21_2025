4853b747ab405b9d418d09cd22cf17655de07135
JessieDomenicActub-UCM <jessiemangz11@gmail.com>
2025-09-16 08:53:55 +0800

    Add files via upload

---
+++ FirstDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+
+    public static void main(String[] args) {
+        // Display a dialog box with a message
+        JOptionPane.showMessageDialog(null, "First Java dialog");
+    }
+}
