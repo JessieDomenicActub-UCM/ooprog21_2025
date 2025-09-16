8d9b8d659ae19f1c01a63addc8175e54c79ebdde
JessieDomenicActub-UCM <jessiemangz11@gmail.com>
2025-09-16 08:56:37 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,21 @@
+/**
+ * This is a Javadoc comment.
+ * It is used for documenting classes, methods, and variables.
+ * Javadoc comments are typically used to generate HTML documentation.
+ */
+public class Comments {
+
+    // This is a single-line comment.
+    // It is used to add short, explanatory notes to the code.
+    public static void main(String[] args) {
+        
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines and is useful for
+         * explaining a block of code or temporarily disabling it.
+         */
+        System.out.println("This program demonstrates all three types of comments in Java.");
+        System.out.println("Single-line, multi-line, and Javadoc.");
+
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+
+    public static void main(String[] args) {
+
+        System.out.println("I spent the whole day in my head do a little sring cleanin");
+        System.out.println("im always to busy dreaming");
+        System.out.println("well maybe i should wake up instead");
+        System.out.println("A lot of things I regret, but I just say I forget");
+
+    }
+}
